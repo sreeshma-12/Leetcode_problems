e7f05d5406a153319617e0943226bdc9511efd77
sreeshma <sreeshma.t.2019@gmail.com>
2024-05-04 16:02:52 +0530

    Two sum

--- workouts.js
+++ workouts.js
@@ -1,11 +1,12 @@
-var isPalindrome = function (x) {
-    let s = x.toString();
-    let str = s.split("").reverse().join("");
-    let int = parseInt(str, 10);
-    if (x === int) {
-        return true;
-    } else {
-        return false;
+var twoSum = function (nums, target) {
+    for (let i = 0; i <= nums.length - 2; i++) {
+        for (let j = 1; j <= nums.length - 1; j++) {
+            if (target == nums[i] + nums[j] && i != j) {
+                return [i, j];
+            }
+        }
     }
+    return null;
 };
-console.log(isPalindrome(123));
+
+twoSum([1, 4, 5, 0, 6], 5);
